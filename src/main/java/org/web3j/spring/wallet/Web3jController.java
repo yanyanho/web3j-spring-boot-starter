3b67fd3c13c6e08e774f3a2ab66c64bb0cb652bf
a1233695 <178225946@qq.com>
2018-04-14 17:15:23 +0800

    [T] update filestream

--- src/main/java/org/web3j/spring/wallet/Web3jController.java
+++ src/main/java/org/web3j/spring/wallet/Web3jController.java
@@ -1,13 +1,20 @@
 package org.web3j.spring.wallet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-
-import java.math.BigDecimal;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class Web3jController {
@@ -19,19 +26,26 @@ public class Web3jController {
         return web3jService.getClientVersion();
     }
 
+//    @RequestMapping(value = "/new-wallet/{password}", method = RequestMethod.GET)
+//    MultipartFile getNewWallert1(@PathVariable String password) throws Exception {
+//        return web3jService.newWallet(password);
+//    }
+
     @RequestMapping(value = "/new-wallet/{password}", method = RequestMethod.GET)
-    String getNewWallert(@PathVariable String password) throws Exception {
-        return web3jService.newWallet(password);
+    ResponseEntity<InputStreamResource> getNewWallert(@PathVariable String password) throws Exception {
+        File file = web3jService.newWallet(password);
+        return ResponseEntity.ok().headers(headers(file)).body(new InputStreamResource(new FileInputStream(file)));
     }
 
-    @RequestMapping(value ="/{address}/balance", method = RequestMethod.GET)
+
+    @RequestMapping(value = "/{address}/balance", method = RequestMethod.GET)
     BigInteger getBalance(@PathVariable String address) {
         return web3jService.getBalance(address);
     }
 
-    @RequestMapping(value="/transfer", method = RequestMethod.GET)
-    TransactionReceipt transfer(@RequestParam  String address , @RequestParam double value, @RequestParam Credentials credentials) throws Exception {
-        return   web3jService.transaction(address, value,  credentials);
+    @RequestMapping(value = "/transfer", method = RequestMethod.GET)
+    TransactionReceipt transfer(@RequestParam String address, @RequestParam double value, @RequestParam Credentials credentials) throws Exception {
+        return web3jService.transaction(address, value, credentials);
     }
 
 
@@ -41,8 +55,18 @@ public class Web3jController {
 //    }
 
 
+    public HttpHeaders headers(File file) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION,"attachment;filename*=UTF-8''"+encode(file.getName()));
+        return httpHeaders;
+    }
 
-
-
-
+    private String encode(String name){
+        try {
+            return URLEncoder.encode(name, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/web3j/spring/wallet/Web3jService.java
+++ src/main/java/org/web3j/spring/wallet/Web3jService.java
@@ -1,23 +1,16 @@
 package org.web3j.spring.wallet;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.admin.Admin;
-import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
@@ -25,6 +18,15 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 
 
 @Service
@@ -38,8 +40,8 @@ public class Web3jService {
 
     //private Credentials credentials;
 
-    @Autowired
-    private Transfer transfer;
+//    @Autowired
+//    private Transfer transfer;
 
 
     private static final Logger log = LoggerFactory.getLogger(Web3jService.class);
@@ -84,16 +86,21 @@ public class Web3jService {
         return web3ClientVersion.getWeb3ClientVersion();
     }
 
-    public String newWallet(String password) throws Exception {
+    public File newWallet(String password) throws Exception {
         String fileName = WalletUtils.generateNewWalletFile(
                 password, new File("src/main/resources/wallet"), false);
-        String[] fetchAddress = fileName.split("--");
-        String getAddress = fetchAddress[fetchAddress.length - 1].split("\\.")[0];
 
-        log.info("walletFileName>>>>>" + fileName.substring(0));
-        log.info("walletFile Address>>>>>" + "0x" + getAddress);
-
-        return fileName;
+//        log.info("walletFileName>>>>>" + "src/main/resources/wallet/"+fileName);
+//        String fileName = "123.xls";
+        File file = new File("src/main/resources/wallet/"+fileName);
+       // FileInputStream inputStream = new FileInputStream(new File("src/main/resources/wallet/"+fileName));
+        return file;
+//        return null;
+//        String[] fetchAddress = fileName.split("--");
+//        String getAddress = fetchAddress[fetchAddress.length - 1].split("\\.")[0];
+//
+//        log.info("walletFileName>>>>>" + fileName.substring(0));
+//        log.info("walletFile Address>>>>>" + "0x" + getAddress);
     }
 
 
@@ -104,12 +111,12 @@ public class Web3jService {
         return nonce;
     }
 
-    public void unlockWallet(String address, String password, String toAddress, double ethBalance) throws ExecutionException, InterruptedException {
-        PersonalUnlockAccount personalUnlockAccount = web3jAdmin.personalUnlockAccount(address, password).sendAsync().get();
-        if (personalUnlockAccount.accountUnlocked()) {
-            transfer.sendFunds(toAddress, BigDecimal.valueOf(ethBalance), Convert.Unit.ETHER);
-        }
-    }
+//    public void unlockWallet(String address, String password, String toAddress, double ethBalance) throws ExecutionException, InterruptedException {
+//        PersonalUnlockAccount personalUnlockAccount = web3jAdmin.personalUnlockAccount(address, password).sendAsync().get();
+//        if (personalUnlockAccount.accountUnlocked()) {
+//            transfer.sendFunds(toAddress, BigDecimal.valueOf(ethBalance), Convert.Unit.ETHER);
+//        }
+//    }
 
 //    public void getJsonFileByAddresee(String address) {
 //        for(File file : )
