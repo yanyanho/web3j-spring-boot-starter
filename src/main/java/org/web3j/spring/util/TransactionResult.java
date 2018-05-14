aecd8f4e1b7982439adcbe8ca46366ecd241e3e7
kongmeijing <kongmj@cmbchina.com>
2018-05-14 09:43:06 +0800

    [T] lombok

--- src/main/java/org/web3j/spring/util/TransactionResult.java
+++ src/main/java/org/web3j/spring/util/TransactionResult.java
@@ -1,36 +1,13 @@
 package org.web3j.spring.util;
 
+import lombok.Data;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 import java.util.List;
 
-
+@Data
 public class TransactionResult {
     String status;
     String message;
     List<TransactionReceiptWithMore> result;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public List<TransactionReceiptWithMore> getResult() {
-        return result;
-    }
-
-    public void setResult(List<TransactionReceiptWithMore> result) {
-        this.result = result;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
